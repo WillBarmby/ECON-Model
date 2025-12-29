42fc8782b7c3123d571cff31f326446a1b0cb24c
Will Barmby <willbarmby@gmail.com>
2025-12-29 12:33:51 -0500

    Add CLI argument for simulation length

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 from config.paths import PARAMETERS, SHOCKS
-from csv_reader import read_parameters, read_shocks
+from csv_reader import read_parameters, validate_params
 from model import find_initial_state, apply_shocks, step
+from params import Params
+from state import State
 import pandas as pd
+import argparse
+
+
+def non_negative_int(value: str) -> int:
+    try:
+        int_value = int(value)
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"{value} is not an integer")
+
+    if int_value < 0:
+        raise argparse.ArgumentTypeError(f"{value} needs to be an integer ≥ 0")
+
+    return int_value
 
 
 if __name__ == "__main__":
-    params = read_parameters(str(PARAMETERS))
-    t_sim = int(params["T_sim"])
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "t",
+        help="number of times simulation runs (default 10)",
+        nargs="?",
+        type=non_negative_int,
+        default=10,
+    )
+    args = parser.parse_args()
+    raw_params = read_parameters(str(PARAMETERS))
+    params: Params = validate_params(raw_params)
+    t_sim = args.t
     df = pd.read_csv(str(SHOCKS)).set_index("t")
-    states = []
+    states: list[State] = []
     state = find_initial_state(params)
     states.append(state)
     for t in range(t_sim):
